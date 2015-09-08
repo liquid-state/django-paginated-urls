4c6d3fa87ae5e8ff255b9ea87b5d9c0d95d218eb
Cyril Doussin <cyril@liquid-state.com>
2015-09-08 23:16:25 +1000

    Add template tag to use as replacement of the standard django "url" tag.

--- paginated_urls/urlresolvers.py
+++ paginated_urls/urlresolvers.py
@@ -6,11 +6,9 @@
 
 from django.core.urlresolvers import reverse
 
+from .utils import add_page_to_url
+
 
 def paginated_reverse(request, url_name, **kwargs):
     url = reverse(viewname=url_name, **kwargs)
-    if request.user.is_authenticated():
-        latest_paginated_url = request.session.get('latest_paginated_url')
-        if latest_paginated_url['url_name'] == url_name:
-            url = '{}?page={}'.format(url, latest_paginated_url['page'])
-    return url
+    return add_page_to_url(request, url_name, url)
