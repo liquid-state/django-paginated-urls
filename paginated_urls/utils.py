8f5d68056419e6b231b6507caa5886ce71a2ad48
Cyril Doussin <cyril@liquid-state.com>
2015-09-28 11:12:12 +1000

    * Support arguments to reverse. * Add ability to force the use of a particular page * Add utils function to override stored page

--- paginated_urls/utils.py
+++ paginated_urls/utils.py
@@ -5,8 +5,14 @@
 """
 
 
-def add_page_to_url(request, url):
+def add_page_to_url(request, url, force_page=None):
     latest_paginated_url = request.session.get('latest_paginated_url')
-    if latest_paginated_url and latest_paginated_url['path'] == url:
-        url = '{}?page={}'.format(url, latest_paginated_url['page'])
+    if force_page or latest_paginated_url.get('path') == url:
+        # if we're using force_page, we're not just trying to get whichever page was the last one
+        # TODO: build query string in a more robust way
+        url = '{}?page={}'.format(url, force_page or latest_paginated_url['page'])
     return url
+
+
+def override_page_for_url(request, url, page):
+    request.session['latest_paginated_url'] = dict(path=url, page=page)
