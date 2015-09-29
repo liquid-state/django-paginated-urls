9360bdff0a3a5d25468c39b33bf53189feae9736
Cyril Doussin <cyril@liquid-state.com>
2015-09-29 10:54:16 +1000

    Check for None

--- paginated_urls/utils.py
+++ paginated_urls/utils.py
@@ -7,7 +7,7 @@
 
 def add_page_to_url(request, url, force_page=None):
     latest_paginated_url = request.session.get('latest_paginated_url')
-    if force_page or latest_paginated_url.get('path') == url:
+    if latest_paginated_url and (force_page or latest_paginated_url.get('path') == url):
         # if we're using force_page, we're not just trying to get whichever page was the last one
         # TODO: build query string in a more robust way
         url = '{}?page={}'.format(url, force_page or latest_paginated_url['page'])
